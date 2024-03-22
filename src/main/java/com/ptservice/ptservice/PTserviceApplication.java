236187fcff2e9b0817b5ebdc55eecc6939f2b442
DESKTOP-JH9P8S7\taehun <ekdh0858@naver.com>
2024-03-22 10:21:01 +0900

    first commit

---
+++ src/main/java/com/ptservice/ptservice/PTserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ptservice.ptservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PTserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PTserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ptservice/ptservice/PTserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptservice.ptservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PTserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
